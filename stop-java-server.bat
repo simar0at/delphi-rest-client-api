16dbfc3befbc0da7ce7ebf8111356f29456498a8
Omar Siam <Omar.Siam@oeaw.ac.at>
2014-10-10 11:56:33 +0200

    Projekte für Delphi 2010 umgestellt.

--- stop-java-server.bat
+++ stop-java-server.bat
@@ -1,2 +1,6 @@
+set JAVA_HOME=C:\Program Files\Java\jdk1.7.0_60
+set M2_HOME=C:\Program Files\Apache Foundation\apache-maven-3.0.4
+set ANT_HOME=C:\Program Files\Apache Foundation\apache-ant-1.9.3
+set PATH=%M2_HOME%\bin;%JAVA_HOME%\bin;%ANT_HOME%\bin;%PATH%
 cd java-rest-server
 mvn jetty:stop
