2af988df8b7a3b288eaccd32e6ade18200e73e8d
Omar Siam <Omar.Siam@oeaw.ac.at>
2016-05-23 14:54:28 +0200

    java 1.8.0_92

--- stop-java-server.bat
+++ stop-java-server.bat
@@ -1,4 +1,4 @@
-set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_60
+set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_92
 set M2_HOME=C:\Program Files\Apache Foundation\apache-maven-3.3.3
 set ANT_HOME=C:\Program Files\Apache Foundation\apache-ant-1.9.6
 set PATH=%M2_HOME%\bin;%JAVA_HOME%\bin;%ANT_HOME%\bin;%PATH%
