1580c62cadb75ce41792966eef0383a7d8cd115c
Omar Siam <Omar.Siam@oeaw.ac.at>
2018-06-11 18:04:50 +0200

    Convert to Java 10, UTF8.

--- stop-java-server.bat
+++ stop-java-server.bat
@@ -1,6 +1,8 @@
-set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_92
-set M2_HOME=C:\Program Files\Apache Foundation\apache-maven-3.3.3
-set ANT_HOME=C:\Program Files\Apache Foundation\apache-ant-1.9.6
-set PATH=%M2_HOME%\bin;%JAVA_HOME%\bin;%ANT_HOME%\bin;%PATH%
+set JAVA_HOME=C:\Program Files\Java\jdk-10.0.1
+set M2_HOME=C:\Program Files\Apache Foundation\apache-maven-3.5.3
+set ANT_HOME=C:\Program Files\Apache Foundation\apache-ant-1.10.3
+set GRADLE_HOME=C:\Program Files\gradle.org\gradle-4.6
+:: set CLASSPATH=.;C:\Program Files\Antlr\antlr-4.5.2-complete.jar;C:\Program Files\Antlr\antlr-3.5.2-complete.jar;%CLASSPATH%
+set PATH=%JAVA_HOME%\bin;%M2_HOME%\bin;%ANT_HOME%\bin;%GRADLE_HOME%\bin;C:\Program Files\Antlr;%PATH%
 cd java-rest-server
 mvn jetty:stop
