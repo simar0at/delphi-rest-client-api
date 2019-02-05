6e3fbe4995e933e556b04e06a67f3a283a2a9785
Omar Siam <simar@gmx.net>
2019-02-05 12:05:44 +0100

    Update unittest java server so it sends correct utf8, jdk 11 as env.

--- start-java-server.bat
+++ start-java-server.bat
@@ -1,8 +1,8 @@
-set JAVA_HOME=C:\Program Files\Java\jdk-10.0.1
-set M2_HOME=C:\Program Files\Apache Foundation\apache-maven-3.5.3
-set ANT_HOME=C:\Program Files\Apache Foundation\apache-ant-1.10.3
-set GRADLE_HOME=C:\Program Files\gradle.org\gradle-4.6
-:: set CLASSPATH=.;C:\Program Files\Antlr\antlr-4.5.2-complete.jar;C:\Program Files\Antlr\antlr-3.5.2-complete.jar;%CLASSPATH%
+set JAVA_HOME=C:\Program Files\Java\jdk-11.0.2
+set M2_HOME=C:\Program Files\Apache Foundation\apache-maven-3.5.4
+set ANT_HOME=C:\Program Files\Apache Foundation\apache-ant-1.10.5
+set GRADLE_HOME=C:\Program Files\gradle.org\gradle-4.10.2
+:: set CLASSPATH=.;C:\Program Files\Antlr\antlr-4.5.2-complete.jar;C:\Program Filesset PATH=%JAVA_HOME%\bin;%M2_HOME%\bin;%ANT_HOME%\bin;%GRADLE_HOME%\bin;C:\Program Files\Antlr;%PATH%
 set PATH=%JAVA_HOME%\bin;%M2_HOME%\bin;%ANT_HOME%\bin;%GRADLE_HOME%\bin;C:\Program Files\Antlr;%PATH%
 cd java-rest-server
 call mvn clean
