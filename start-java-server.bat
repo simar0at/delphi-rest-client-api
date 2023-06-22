9799073c2be04288607778f035e2fb3bbc64e7d1
Omar Siam <Omar.Siam@oeaw.ac.at>
2023-06-22 20:36:47 +0200

    Use scoop paths for java apps

--- start-java-server.bat
+++ start-java-server.bat
@@ -1,7 +1,7 @@
-set JAVA_HOME=C:\Program Files\Zulu\zulu-11
-set M2_HOME=C:\Program Files\Apache Foundation\apache-maven-3.9.1
-set ANT_HOME=C:\Program Files\Apache Foundation\apache-ant-1.10.13
-set GRADLE_HOME=C:\Program Files\gradle.org\gradle-4.10.2
+set JAVA_HOME=%HOMEPATH%\scoop\apps\zulufx11-jdk\current
+set M2_HOME=%HOMEPATH%\scoop\apps\maven\current
+set ANT_HOME=%HOMEPATH%\scoop\apps\ant\current
+set GRADLE_HOME=%HOMEPATH%\scoop\apps\gradle\current
 :: set CLASSPATH=.;C:\Program Files\Antlr\antlr-4.5.2-complete.jar;C:\Program Filesset PATH=%JAVA_HOME%\bin;%M2_HOME%\bin;%ANT_HOME%\bin;%GRADLE_HOME%\bin;C:\Program Files\Antlr;%PATH%
 set PATH=%JAVA_HOME%\bin;%M2_HOME%\bin;%ANT_HOME%\bin;%GRADLE_HOME%\bin;C:\Program Files\Antlr;%PATH%
 cd java-rest-server
