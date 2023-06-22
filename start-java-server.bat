0d2a1e08d82cd5df1efd40c71031cc38cafccb56
Omar Siam <Omar.Siam@oeaw.ac.at>
2023-06-22 18:53:53 +0200

    Versions and JDK provider

--- start-java-server.bat
+++ start-java-server.bat
@@ -1,6 +1,6 @@
-set JAVA_HOME=C:\Program Files\Java\jdk-11.0.2
-set M2_HOME=C:\Program Files\Apache Foundation\apache-maven-3.5.4
-set ANT_HOME=C:\Program Files\Apache Foundation\apache-ant-1.10.5
+set JAVA_HOME=C:\Program Files\Zulu\zulu-11
+set M2_HOME=C:\Program Files\Apache Foundation\apache-maven-3.9.1
+set ANT_HOME=C:\Program Files\Apache Foundation\apache-ant-1.10.13
 set GRADLE_HOME=C:\Program Files\gradle.org\gradle-4.10.2
 :: set CLASSPATH=.;C:\Program Files\Antlr\antlr-4.5.2-complete.jar;C:\Program Filesset PATH=%JAVA_HOME%\bin;%M2_HOME%\bin;%ANT_HOME%\bin;%GRADLE_HOME%\bin;C:\Program Files\Antlr;%PATH%
 set PATH=%JAVA_HOME%\bin;%M2_HOME%\bin;%ANT_HOME%\bin;%GRADLE_HOME%\bin;C:\Program Files\Antlr;%PATH%
